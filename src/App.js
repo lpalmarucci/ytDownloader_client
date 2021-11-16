219f19d5226c33ffb77f3d0bbbe85b981b20a534
Luca Palmarucci <lpalmarucci@gmail.com>
2021-11-16 16:48:34 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import {React, useState, useEffect} from "react";
+import ReactDom from 'react-dom'
 import './App.css';
+import imgFileDownloaded from './img/check.svg';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+
+  const [file, setFile] = useState({});
+  const [url, setUrl] =useState("");
+  var [downloadSuccessStyle, setDownloadSuccessStyle] = useState({opacity: 0});
+
+  useEffect(() => {
+
+  }, []);
+
+  const handleClick = () => {
+    window.open(`http://localhost:3001/download?url=${url}`);
+    setDownloadSuccessStyle({opacity: 1});
+    setTimeout(() => {
+      setDownloadSuccessStyle({opacity: 0});
+    },5000);
+    setUrl("");
+  }
+
+  const handleChange = (event) => setUrl(event.target.value);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h1 id="title">Youtube Downloader</h1>
+        <div id="main">
+            <input type="text" id="searchbox" name="url" onChange={handleChange} value={url}/>
+            <button type="button" id="convertNow" onClick={handleClick}>Convert now</button> 
+        </div>
+        <div id="downloadSuccess" style={downloadSuccessStyle}>
+            <img src={imgFileDownloaded} height="50" width="50"/>
+            <p>Download started!</p>
+        </div>
     </div>
   );
 }
