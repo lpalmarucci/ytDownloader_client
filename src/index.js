e81e3aceaecb1bba6919333c4faebde9aef86de9
Luca Palmarucci <lpalmarucci@gmail.com>
2021-11-19 17:56:19 +0100

    Rename rendering App component in Download

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Download from './Download';
 import Login from './components/Login';
 import Register from './components/Register.js';
 import './css/media.css';
+import './css/index.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Switch>
-        <Route path="/" exact component={App} />
+        <Route path="/" exact component={Download} />
         <Route path="/login" exact component={Login} />
         <Route path="/register" exact component={Register} />
       </Switch>
     </Router>
-    {/* <App /> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
