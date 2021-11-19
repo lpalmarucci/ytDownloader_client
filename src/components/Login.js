1dc678068b0336c438efad479341cdeb395e96dc
Luca Palmarucci <lpalmarucci@gmail.com>
2021-11-19 13:06:54 +0100

    Login component with authentication request complete

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,53 @@
 import React, { Component } from 'react'
 import '../css/Login.css'
+import axios from 'axios'
+import { withRouter } from 'react-router';
+import { Link } from 'react-router-dom';
+import { checkResponseStatus } from '../lib/request';
 
-export default class Login extends Component {
-    constructor() {
-        super();
+class Login extends Component {
+    constructor(props) {
+        super(props);
         this.state = {
             username: '',
             password: ''
         }
-
-        this.handleUsernameChange = this.handleUsernameChange.bind(this);
-        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmitLoginForm = this.handleSubmitLoginForm.bind(this);
     }
 
-    handleUsernameChange(e) {
-        this.setState({ username: e.target.value });
-    }
+    handleSubmitLoginForm(e) {
+        e.preventDefault();
+        axios({
+            method: 'POST',
+            data: {
+                username: this.state.username,
+                password: this.state.password
+            },
+            withCredentials: true,
+            url: 'http://localhost:3001/login'
+        }).then(res => {
+            if (checkResponseStatus(res.data)) {
+                this.props.history.push('/');
+            } else {
+                console.error(res.data.body.errorMessage)
+            }
+        })
 
-    handlePasswordChange(e) {
-        this.setState({ password: e.target.value });
     }
 
     render() {
+
         return (
             <section className="login-section">
-                <h1>Login</h1>
-                <form className="login-form">
-                    <input type="text" placeholder="Username" value={this.state.username} onChange={this.handleUsernameChange} />
-                    <input type="password" placeholder="Password" value={this.state.password} onChange={this.handlePasswordChange} />
-                    <button type="button">Submit</button>
+                <h1>Login (or <Link to='/register'>register</Link>)</h1>
+                <form className="login-form" onSubmit={this.handleSubmitLoginForm}>
+                    <input type="text" placeholder="Username" value={this.state.username} onChange={(e) => this.setState({ username: e.target.value })} />
+                    <input type="password" placeholder="Password" value={this.state.password} onChange={(e) => this.setState({ password: e.target.value })} />
+                    <button type="submit" className="login-button" >Submit</button>
                 </form>
             </section>
         )
     }
 }
+
+export default withRouter(Login);
